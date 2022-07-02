ff0a90bdcced93e0b9ef4fd01243b4475cab0ea3
Gaston Leegstra <gastonleegstra@gmail.com>
2022-07-02 20:21:50 -0300

    se corrige admin de manage_product

--- manage_product/admin.py
+++ manage_product/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from manage_product.models import Categoria, Cerveza,Envase, Brew
 
 # Register your models here.
-@admin.site.register(Cerveza)
+@admin.register(Cerveza)
 class CervezaAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'precio', 'tipo', 'brew','color','envase','categoria','activo']
+
 admin.site.register(Categoria)
 admin.site.register(Brew)
 admin.site.register(Envase)
