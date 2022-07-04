c7c91f337c7cb9a274acf6ba5900c86c1b95fcfa
user.name like : "Gaston Leegstra <user.email like : "gastonleegstra@gmail.com>
2022-07-04 16:53:41 -0300

    se modifica foto de perfil

--- users/models.py
+++ users/models.py
@@ -1,6 +1,4 @@
 from distutils.command.upload import upload
-from pyexpat import model
-from statistics import mode
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -13,3 +11,6 @@ class User_profile(models.Model):
     apellido = models.CharField(max_length=20,default = "")
     correo = models.CharField(max_length=20,default = "")
     profile_image = models.ImageField(upload_to='profile_image',default = "")
+
+    def __str__(self):
+        return self.user
